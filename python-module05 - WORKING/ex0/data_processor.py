f65c5cd8ebe139b693ce8c0b8c6347db7e1b7c10
n5ssim <nassimc02@gmail.com>
2026-04-14 16:41:34 +0200

    check mypy, flake8

--- python-module05 - WORKING/ex0/data_processor.py
+++ python-module05 - WORKING/ex0/data_processor.py
@@ -3,10 +3,11 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
     def __init__(self) -> None:
-        self._data : list[str] = []
-        self._rank : int = 0
+        self._data: list[str] = []
+        self._rank: int = 0
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -29,8 +30,8 @@ class NumericProcessor(DataProcessor):
 
     def validate(self, data: Any) -> bool:
         return isinstance(data, (int, float)) or (
-            isinstance(data, list) and all(isinstance(x, (int, float)) for x in data))
-    
+            isinstance(data, list) and
+            all(isinstance(x, (int, float)) for x in data))
 
     def ingest(self, data: int | float | list[int | float]) -> None:
         if not self.validate(data):
@@ -47,7 +48,7 @@ class TextProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
         return isinstance(data, str) or (
             isinstance(data, list) and all(isinstance(x, str) for x in data))
-    
+
     def ingest(self, data: str | list[str]) -> None:
         if not self.validate(data):
             raise TypeError("improper text data")
@@ -62,18 +63,21 @@ class LogProcessor(DataProcessor):
 
     def _is_valid_dict(self, data: Any) -> bool:
         return isinstance(data, dict) and all(
-            isinstance (k, str) and isinstance(v, str) for k, v in data.items())
-    
+            isinstance(k, str) and isinstance(v, str)
+            for k, v in data.items())
+
     def validate(self, data: Any) -> bool:
         return self._is_valid_dict(data) or (
-            isinstance(data, list) and all(self._is_valid_dict(x) for x in data))
-    
+            isinstance(data, list) and
+            all(self._is_valid_dict(x) for x in data))
+
     def ingest(self, data: dict | list[dict]) -> None:
         if not self.validate(data):
             raise TypeError("improper log data")
         if isinstance(data, list):
             for item in data:
-                self._data.append(f"{item['log_level']}: {item['log_message']}")
+                self._data.append(f"{item['log_level']}:"
+                                  f" {item['log_message']}")
         else:
             self._data.append(f"{data['log_level']}: {data['log_message']}")
 
@@ -81,7 +85,7 @@ class LogProcessor(DataProcessor):
 if __name__ == "__main__":
     print("=== Code Nexus - Data Processor ===")
 
-    print(f"\nTest Numeric Processor...")
+    print("\nTest Numeric Processor...")
     numeric = NumericProcessor()
 
     print(f"Trying to validate input '42': {numeric.validate(42)}")
@@ -93,7 +97,7 @@ if __name__ == "__main__":
     except TypeError as e:
         print(f"Got exception: {e}")
 
-    print(f"Processing data: [1, 2, 3, 4, 5]")
+    print("Processing data: [1, 2, 3, 4, 5]")
     numeric.ingest([1, 2, 3, 4, 5])
 
     print("Extracting 3 values...")
@@ -107,7 +111,7 @@ if __name__ == "__main__":
 
     print(f"Trying to validate input '42': {text.validate(42)}")
 
-    print(f"Processing data: ['Hello', 'Nexus', 'World']")
+    print("Processing data: ['Hello', 'Nexus', 'World']")
     text.ingest(["Hello", "Nexus", "World"])
 
     print("Extracting 1 value...")
